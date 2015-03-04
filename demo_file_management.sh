c78bba260ff8bbb33a77c7d18448a882f4c73e31
hurngchunlee <h.lee@donders.ru.nl>
2015-03-04 16:40:12 +0100

    demonstrate uploading README file

--- demo_file_management.sh
+++ demo_file_management.sh
@@ -10,6 +10,10 @@ echo "======> 1-2. upload local file 'data/fzero.100mb' to path 'raw_data/fzero.
 read
 ./file_upload.py -u demo_user -p raw_data/fzero.100mb.1 data/fzero.100mb dac_00002
 
+echo "======> 1-3. upload annotation 'data/README' to collection 'dac_00002' ..."
+read
+./file_upload.py -u demo_user data/README dac_00002
+
 echo "====> 2. List file 'raw_data/fzero.100mb.1' in collection dac_00002 ..."
 read
 ./file_list.py -u demo_user -p raw_data/fzero.100mb.1 dac_00002
